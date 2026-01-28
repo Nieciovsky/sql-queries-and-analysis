a3961a3fecb69dad2e785a77e3c4ea16f88fb6fe
Kamil Nieć <kamil.niec@int.pl>
2026-01-28 21:42:42 +0100

    Update 01_select.sql

--- queries/01_select.sql
+++ queries/01_select.sql
@@ -1,1 +1,11 @@
+-- Task 1: Select all employees
+SELECT *
+FROM employees;
 
+-- Task 2: Select specific columns (first name, last name, department)
+SELECT first_name, last_name, department
+FROM employees;
+
+-- Task 3: Select employees with salary and hire date
+SELECT first_name, last_name, salary, hire_date
+FROM employees;
