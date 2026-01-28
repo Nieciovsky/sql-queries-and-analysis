da1e723e32bb600fade950d35d34bc8500e7a204
Kamil Nieć <kamil.niec@int.pl>
2026-01-28 21:44:18 +0100

    Update 03_order_by.sql

--- queries/03_order_by.sql
+++ queries/03_order_by.sql
@@ -1,1 +1,9 @@
+-- Employees ordered by salary ascending
+SELECT *
+FROM employees
+ORDER BY salary;
 
+-- Employees ordered by hire date descending
+SELECT *
+FROM employees
+ORDER BY hire_date DESC;
