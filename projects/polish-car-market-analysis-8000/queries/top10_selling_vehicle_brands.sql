7da1bf11f822852fc08ed3923095ab6dfa5ec978
Kamil Nieć <kamil.niec@int.pl>
2026-01-29 21:12:18 +0100

    Update top10_selling_vehicle_brands.sql

--- projects/polish-car-market-analysis-8000/queries/top10_selling_vehicle_brands.sql
+++ projects/polish-car-market-analysis-8000/queries/top10_selling_vehicle_brands.sql
@@ -2,7 +2,8 @@
 
 SELECT 
     Vehicle_brand,
-    COUNT(*) AS sold_count
+    COUNT(*) AS sold_count,
+ROUND(AVG(price), 2) AS avg_price
 FROM car_data
 GROUP BY Vehicle_brand
 ORDER BY sold_count DESC
